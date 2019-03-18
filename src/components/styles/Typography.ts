5e84157146b51f2c402849e0d646c063d032527f
Shah <shahthepro@outlook.com>
2019-03-18 09:32:23 +0530

    Add title to MainPage

--- src/components/styles/Typography.ts
+++ src/components/styles/Typography.ts
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 
-const Title = styled.h1`
+interface ITitleProps {
+  centered?: boolean,
+}
+
+const Title = styled.h1<ITitleProps>`
   font-weight: 100;
   margin: 0;
   padding-bottom: 1rem;
+  text-align: ${props => props.centered ? 'center' : 'left' };
 `;
 
 const Subtitle = styled.h2`
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Container from 'src/components/styles/Container';
 import BlockExplorer from 'src/components/BlockExplorer';
+import { Title } from 'src/components/styles/Typography';
 
 class MainPage extends React.Component {
   render() {
     return (
       <Container>
+        <Title centered>Block Explorer</Title>
         <BlockExplorer />
       </Container>
     );
