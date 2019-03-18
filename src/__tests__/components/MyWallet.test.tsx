2962fdda1f875f129c62e4c5a3635d7a54b552fe
Shah <shahthepro@outlook.com>
2019-03-18 22:58:13 +0530

    Add tests for MyWallet component

---
+++ src/__tests__/components/MyWallet.test.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import MyWallet from './../../components/MyWallet';
+import * as Enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+import mountAndMatchSnapshotHOF from '../../../mountAndMatchSnapshotHOF';
+
+Enzyme.configure({ adapter: new Adapter() })
+const { mount } = Enzyme;
+
+const mountAndMatchSnapshot = mountAndMatchSnapshotHOF(it, ReactDOM, mount);
+
+// @ts-ignore
+mountAndMatchSnapshot('MyWallet wallet={isConnected: true}', <MyWallet wallet={{
+  isConnected: true,
+  isConnecting: false,
+  networkId: Number(1),
+  address: '1234567890abcdef',
+  connect: () => {},
+  disconnect: () => {},
+}} />);
+
+// @ts-ignore
+mountAndMatchSnapshot('MyWallet wallet={isConnected: false}', <MyWallet wallet={{
+  isConnected: false,
+}} />);
