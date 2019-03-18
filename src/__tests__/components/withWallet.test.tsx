e00942d7c77edb0b8f628dbc5c1f5ab0e026a03a
Shah <shahthepro@outlook.com>
2019-03-18 22:49:15 +0530

    Add unit test for withWallet HOC

---
+++ src/__tests__/components/withWallet.test.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import withWallet from './../../components/withWallet';
+import * as Enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() })
+const { mount } = Enzyme;
+
+it(`renders components and passes down wallet as prop `, () => {
+  // const element = <div />;
+  class fakeElement extends React.Component {
+    render() {
+      return (
+        <div>Hello</div>
+      );
+    }
+  }
+  const WrappedElement = withWallet(fakeElement); 
+  // @ts-ignore
+  const mountedElement = mount(<WrappedElement />);
+  // @ts-ignore
+  expect(mountedElement.children().props().wallet).toBeDefined();
+});
