42958c74f5f8635e29504ad39d9c2c2a58f86c8f
Shah <shahthepro@outlook.com>
2019-03-19 21:49:16 +0530

    Spacing fix

--- src/components/styles/Typography.ts
+++ src/components/styles/Typography.ts
@@ -7,13 +7,13 @@ interface ITitleProps {
 const Title = styled.h1<ITitleProps>`
   font-weight: 100;
   margin: 0;
-  padding-bottom: 1rem;
+  padding-bottom: 2rem;
   text-align: ${props => props.centered ? 'center' : 'left' };
 `;
 
 const Subtitle = styled.h2`
   font-weight: 100;
-  margin: 0;
+  margin: 0 0 1rem 0;
   padding-bottom: 1rem;
 `;
 
